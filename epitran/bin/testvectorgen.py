e7918ad1bb2c5db78afc3cc1a7d5a6eb4e69c5ad
David R. Mortensen <davidmortensen@gmail.com>
2016-04-21 19:19:37 -0400

    Added test script for vector

---
+++ epitran/bin/testvectorgen.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import codecs
+import epitran.vector
+
+def main(code, space, infile):
+    vec = epitran.vector.VectorWithIPASpace(code, space)
+    with codecs.open(infile, 'r', 'utf-8') as f:
+        for line in f:
+            fields = line.split('\t')
+            if len(fields) > 1:
+                word = fields[0]
+                cat, case, orth, phon, vec = vec.word_to_pfvecter(word)
+                print("Category: {}".format(cat))
+                print("Case: {}".format(case))
+                print("Orthographic: {}".format(orth))
+                print("Phonetic: {}".format(phon))
+                print("Vector: {}".format(vec))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--code', help='Script code.')
+    parser.add_argument('-s', '--space', help='Space.')
+    parser.add_argument('-i', '--infile', help='Input file.')
+    args = parser.parse_args()
+    main(args.code, args.space, args.infile)
