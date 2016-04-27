2e8ac5b579c50cc5d62a515cd17bf034ddb1c872
David R. Mortensen <davidmortensen@gmail.com>
2016-04-27 17:59:25 -0400

    Made string literals unicode

--- epitran/test_vector.py
+++ epitran/test_vector.py
@@ -14,7 +14,6 @@ class TestTurkish(unittest.TestCase):
     def testPunc(self):
         punc_words = [u'"', u"'", u'.', u',', u':', u';', u"‘", u"’", u"”", u"“", u"。", u"，"]
         for word in punc_words:
-            print(u'Item:\t{}'.format(word))
             segs = self.vwis.word_to_segs(word)
             self.assertEqual(segs[0][0], u'P')
             self.assertEqual(segs[0][1], 0)
@@ -23,24 +22,23 @@ class TestTurkish(unittest.TestCase):
     def testPuncLike(self):
         punc_words = [u"ʼ"]
         for word in punc_words:
-            print(u'Item:\t{}'.format(word))
             segs = self.vwis.word_to_segs(word)
-            self.assertNotEqual(segs[0][0], u'P')
+            self.assertEqual(segs[0][0], u'P')
 
     def testHaziranda(self):
         word = u'Haziranʼda'
         segs = self.vwis.word_to_segs(word)
         marks = [s[:4] for s in segs]
-        correct = [('L', 1, 'H', u'h'),
-                   ('L', 0, 'a', u'a'),
-                   ('L', 0, 'z', u'z'),
-                   ('L', 0, 'i', u'i'),
-                   ('L', 0, 'r', u'ɾ'),
-                   ('L', 0, 'a', u'a'),
-                   ('L', 0, 'n', u'n'),
-                   ('P', 0, "'", u''),
-                   ('L', 0, 'd', u'd'),
-                   ('L', 0, 'a', u'a'), ]
+        correct = [(u'L', 1, u'H', u'h'),
+                   (u'L', 0, u'a', u'a'),
+                   (u'L', 0, u'z', u'z'),
+                   (u'L', 0, u'i', u'i'),
+                   (u'L', 0, u'r', u'ɾ'),
+                   (u'L', 0, u'a', u'a'),
+                   (u'L', 0, u'n', u'n'),
+                   (u'P', 0, u"'", u''),
+                   (u'L', 0, u'd', u'd'),
+                   (u'L', 0, u'a', u'a'), ]
         for m, c in zip(marks, correct):
             self.assertEqual(m, c)
 
@@ -48,15 +46,23 @@ class TestTurkish(unittest.TestCase):
         word = u'Haziranʼda'
         segs = self.vwis.word_to_segs(word)
         marks = [s[:4] for s in segs]
-        correct = [('L', 1, 'H', u'h'),
-                   ('L', 0, 'a', u'a'),
-                   ('L', 0, 'z', u'z'),
-                   ('L', 0, 'i', u'i'),
-                   ('L', 0, 'r', u'ɾ'),
-                   ('L', 0, 'a', u'a'),
-                   ('L', 0, 'n', u'n'),
-                   ('P', 0, "'", u''),
-                   ('L', 0, 'd', u'd'),
-                   ('L', 0, 'a', u'a'), ]
+        correct = [(u'L', 1, u'H', u'h'),
+                   (u'L', 0, u'a', u'a'),
+                   (u'L', 0, u'z', u'z'),
+                   (u'L', 0, u'i', u'i'),
+                   (u'L', 0, u'r', u'ɾ'),
+                   (u'L', 0, u'a', u'a'),
+                   (u'L', 0, u'n', u'n'),
+                   (u'P', 0, u"'", u''),
+                   (u'L', 0, u'd', u'd'),
+                   (u'L', 0, u'a', u'a'), ]
         for m, c in zip(marks, correct):
             self.assertEqual(m, c)
+
+    class TestUzbek(unittest.TestCase):
+        def setUp(self):
+            self.vwis = vector.VectorsWithIPASpace('tur-Latn',
+                                                   'tur-with_attached_suffixes-space')
+
+        def test_apostrophe_letter(self):
+            pass
