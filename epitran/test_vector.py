94fcffc9580ae42f8a7e79f595b04318152fe2e5
David R. Mortensen <davidmortensen@gmail.com>
2016-04-28 17:25:08 -0400

    Added tests

--- epitran/test_vector.py
+++ epitran/test_vector.py
@@ -5,6 +5,9 @@ from __future__ import print_function
 import unittest
 import vector
 
+def map_slice(xs, start, end):
+    return [x[start:end] for x in xs]
+
 
 class TestTurkish(unittest.TestCase):
     def setUp(self):
@@ -12,9 +15,11 @@ class TestTurkish(unittest.TestCase):
                                                'tur-with_attached_suffixes-space')
 
     def testPunc(self):
-        punc_words = [u'"', u"'", u'.', u',', u':', u';', u"‘", u"’", u"”", u"“", u"。", u"，"]
+        punc_words = [u'"', u"'", u'.', u',', u':', u';', u"‘", u"’", u"”", u"“", u"，"]
         for word in punc_words:
+            print(u'Punctuation mark:\t{}'.format(word))
             segs = self.vwis.word_to_segs(word)
+            print(segs)
             self.assertEqual(segs[0][0], u'P')
             self.assertEqual(segs[0][1], 0)
             self.assertIn(segs[0][3], u"\"';,.")
@@ -59,10 +64,19 @@ class TestTurkish(unittest.TestCase):
         for m, c in zip(marks, correct):
             self.assertEqual(m, c)
 
-    class TestUzbek(unittest.TestCase):
-        def setUp(self):
-            self.vwis = vector.VectorsWithIPASpace('tur-Latn',
-                                                   'tur-with_attached_suffixes-space')
+class TestUzbek(unittest.TestCase):
+    def setUp(self):
+        self.vwis = vector.VectorsWithIPASpace('tur-Latn',
+                                               'tur-with_attached_suffixes-space')
 
-        def test_apostrophe_letter(self):
-            pass
+    def test_apostrophe_letter(self):
+        target = [(u'L', 0, u"'", u'')]
+        test = self.vwis.word_to_segs(u"ʼ")
+        print(test)
+        self.assertEqual(map_slice(test, 0, 4), target)
+
+    def test_apostrophe_punc(self):
+        target = [(u'P', 0, u"'", u'')]
+        test = self.vwis.word_to_segs(u"'")
+        print(test)
+        self.assertEqual(map_slice(test, 0, 4), target)
