c83556cfb6eadd5cf7b31e2f498f08222683239e
David R. Mortensen <davidmortensen@gmail.com>
2016-04-29 12:07:59 -0400

    Ensure that cat is 'P' if it is in self.puncnorm

--- epitran/_epitran.py
+++ epitran/_epitran.py
@@ -136,6 +136,7 @@ class Epitran(object):
                 span = word[0]
                 span = self.normalize_punc(span) if normpunc else span
                 cat, case = cat_and_cap(span)
+                cat = 'P' if cat in self.puncnorm else cat
                 phon = u''
                 vecs = to_vectors(phon)
                 tuples.append((cat, case, span, phon, vecs))
