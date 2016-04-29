d6921ef2964864bffc0eeac7c41ae41d181b6a48
David R. Mortensen <dmortens@cs.cmu.edu>
2016-04-29 00:09:55 -0400

    Added tests

--- epitran/test_epitran.py
+++ epitran/test_epitran.py
@@ -6,6 +6,26 @@ import unittest
 import _epitran
 
 
+def map_slice(xs, start, end):
+    return [x[start:end] for x in xs]
+
+class TestNormalizePunc(unittest.TestCase):
+    def setUp(self):
+        self.epi = _epitran.Epitran(u'tur-Latn')
+
+    def test_modifier_letter_apostrophe(self):
+        self.assertEqual(self.epi.normalize_punc(u'\u02bc'), "'")
+
+    def test_left_apostrophe(self):
+        self.assertEqual(self.epi.normalize_punc(u'\u2018'), "'")
+
+    def test_right_apostrophe(self):
+        self.assertEqual(self.epi.normalize_punc(u'\u2019'), "'")
+
+    def test_modifier_letter_turned_comma(self):
+        self.assertEqual(self.epi.normalize_punc(u'\u02bb'), "'")
+
+
 class TestTurkish(unittest.TestCase):
     def setUp(self):
         self.epi = _epitran.Epitran(u'tur-Latn')
@@ -32,9 +52,57 @@ class TestTurkish(unittest.TestCase):
         self.assertEqual(self.epi.transliterate(u'’', normpunc=True), u'\'')
         self.assertEqual(self.epi.transliterate(u'‘', normpunc=True), u'\'')
 
-    def test_robust_trans_pairs(self):
-        self.assertEqual(self.epi.robust_trans_pairs(u'’'), [(u"'", u'')])
-        self.assertEqual(self.epi.robust_trans_pairs(u'du’da'), [(u'd', u'd'), (u'u', u'u'), (u'’', u''), (u'd', u'd'), (u'a', u'a')])
+    def test_word_to_tuples1(self):
+        target = [(u'L', 1, u'H', u'h'),
+                  (u'L', 0, u'a', u'a'),
+                  (u'L', 0, u'z', u'z'),
+                  (u'L', 0, u'i', u'i'),
+                  (u'L', 0, u'r', u'ɾ'),
+                  (u'L', 0, u'a', u'a'),
+                  (u'L', 0, u'n', u'n'),
+                  (u'L', 0, u'ʼ', u''),
+                  (u'L', 0, u'd', u'd'),
+                  (u'L', 0, u'a', u'a')]
+        word = u'Haziranʼda'
+        test = map_slice(self.epi.word_to_tuples(word), 0, 4)
+        self.assertEqual(test, target)
+
+    def test_word_to_tuples2(self):
+        target = [(u'L', 1, u'H', u'h'),
+                  (u'L', 0, u'a', u'a'),
+                  (u'L', 0, u'z', u'z'),
+                  (u'L', 0, u'i', u'i'),
+                  (u'L', 0, u'r', u'ɾ'),
+                  (u'L', 0, u'a', u'a'),
+                  (u'L', 0, u'n', u'n'),
+                  (u'P', 0, u"'", u''),
+                  (u'L', 0, u'd', u'd'),
+                  (u'L', 0, u'a', u'a')]
+        word = u'Haziranʼda'
+        test = map_slice(self.epi.word_to_tuples(word, normpunc=True), 0, 4)
+        self.assertEqual(test, target)
+
+
+class TestUzbek(unittest.TestCase):
+    def setUp(self):
+        self.epi = _epitran.Epitran(u'uzb-Latn')
 
     def test_word_to_tuples(self):
-        self.assertEqual(self.epi.word_to_tuples(u'Hazeran’da')[0][:2], (u'L', 1))
+        target = [(u'L', 0, u'oʻ', u'o'),
+                  (u'L', 0, u'z', u'z'),
+                  (u'L', 0, u'i', u'i'),
+                  (u'L', 0, u'n', u'n'),
+                  (u'L', 0, u'i', u'i')]
+        test = self.epi.word_to_tuples(u'oʻzini')
+        self.assertEqual(map_slice(test, 0, 4), target)
+
+    def test_word_to_tuples2(self):
+        target = [(u'L', 1, u'B', u'b'),
+                  (u'L', 0, u'a', u'a'),
+                  (u'L', 0, u'l', u'l'),
+                  (u'L', 0, u'o', u'ɒ'),
+                  (u'L', 0, u'gʻ', u'ʁ'),
+                  (u'L', 0, u'a', u'a'),
+                  (u'L', 0, u't', u't̪')]
+        test = self.epi.word_to_tuples(u'Balogʻat')
+        self.assertEqual(map_slice(test, 0, 4), target)
