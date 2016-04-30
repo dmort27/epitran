aa84953a75f6cfa8cf073acbe81e89a7218c8f62
David R. Mortensen <davidmortensen@gmail.com>
2016-04-29 21:36:01 -0400

    Made testvectorgen.py safe for Unicode

--- epitran/bin/testvectorgen.py
+++ epitran/bin/testvectorgen.py
@@ -4,26 +4,26 @@ from __future__ import print_function
 
 import argparse
 import codecs
+
 import epitran.vector
 
 
 def main(code, space, infile):
-    vec = epitran.vector.VectorWithIPASpace(code, space)
+    vec = epitran.vector.VectorsWithIPASpace(code, space)
     with codecs.open(infile, 'r', 'utf-8') as f:
         for line in f:
             fields = line.split('\t')
             if len(fields) > 1:
                 word = fields[0]
-                print(u"WORD: {}".format(word))
+                print(u"WORD: {}".format(word).encode('utf-8'))
                 segs = vec.word_to_segs(word)
-                print(segs)
                 for record in segs:
                     cat, case, orth, phon, id_, vector = record
-                    print(u"Category: {}".format(cat))
-                    print(u"Case: {}".format(case))
-                    print(u"Orthographic: {}".format(orth))
-                    print(u"Phonetic: {}".format(phon))
-                    print(u"Vector: {}".format(vector))
+                    print(u"Category: {}".format(cat).encode('utf-8'))
+                    print(u"Case: {}".format(case).encode('utf-8'))
+                    print(u"Orthographic: {}".format(orth).encode('utf-8'))
+                    print(u"Phonetic: {}".format(phon).encode('utf-8'))
+                    print(u"Vector: {}".format(vector).encode('utf-8'))
 
 
 if __name__ == '__main__':
