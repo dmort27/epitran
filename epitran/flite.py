3a74a2c87e55b899329bfd79e9b7de2a9569fc48
David R. Mortensen <davidmortensen@gmail.com>
2016-05-26 15:38:58 -0400

    Added shell interface to flite speech synthesizer for g2p

---
+++ epitran/flite.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+import subprocess
+import os.path
+import unicodecsv as csv
+
+import pkg_resources
+
+
+class Flite(object):
+    def __init__(self, darpabet='darpabet'):
+        darpabet = pkg_resources.resource_filename(__name__, os.path.join('data', darpabet + '.csv'))
+        self.darpa_map = self._read_darpabet(darpabet)
+
+    def _read_darpabet(self, darpabet):
+        darpa_map = {}
+        with open(darpabet, 'rb') as f:
+            reader = csv.reader(f, encoding='utf-8')
+            for darpa, ipa in reader:
+                darpa_map[darpa] = ipa
+        return darpa_map
+
+    def darpa_to_ipa(self, darpa_text):
+        darpa_text = darpa_text.strip()
+        darpa_list = darpa_text.split(' ')[1:-1]  # remove pauses
+        ipa_list = map(lambda d: self.darpa_map[d], darpa_list)
+        return ''.join(ipa_list)
+
+    def english_g2p(self, text):
+        darpa_text = subprocess.check_output(['flite', '-ps', '-o', '/dev/null', '-t', '"{}"'.format(text)])
+        return self.darpa_to_ipa(darpa_text)
