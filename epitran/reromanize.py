0093a70f72b142d03e8c3f30115f68268ac74434
David R. Mortensen <davidmortensen@gmail.com>
2016-07-21 13:54:07 -0400

    Added the reromanize.ReRomanizer class

---
+++ epitran/reromanize.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+
+import os.path
+import sys
+
+import pkg_resources
+
+import epitran
+import unicodecsv as csv
+
+
+class ReRomanizer(object):
+    def __init__(self, code, table):
+        self.epi = epitran.Epitran(code)
+        self.mapping = self._load_reromanizer(table)
+
+    def _load_reromanizer(self, table):
+        path = os.path.join('data', 'reromanize', table + '.csv')
+        try:
+            path = pkg_resources.resource_filename(__name__, path)
+        except:
+            print('Could not locate {}.'.format(path), file=sys.stderr)
+        if os.path.isfile(path):
+            mapping = {}
+            with open(path, 'rb') as f:
+                reader = csv.reader(f, encoding='utf-8')
+                next(reader)
+                for ipa, rom in reader:
+                    mapping[ipa] = rom
+            return mapping
+        else:
+            print('File {} does not exist.'.format(path), file=sys.stderr)
+            return {}
+
+    def reromanize_ipa(self, tr_list):
+        re_rom_list = []
+        for seg in tr_list:
+            if seg in self.mapping:
+                re_rom_list.append(self.mapping[seg])
+            else:
+                re_rom_list.append(seg)
+        return re_rom_list
+
+    def reromanize(self, text):
+        tr_list = self.epi.trans_list(text)
+        return ''.join(self.reromanize_ipa(tr_list))
