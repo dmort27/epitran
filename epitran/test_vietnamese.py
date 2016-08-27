23f744d409b55cf21340f8a6eb52bde989ef1380
David R. Mortensen <dmortens@cs.cmu.edu>
2016-08-27 09:47:50 -0400

    Corrected vietnamese tests

--- epitran/test_vietnamese.py
+++ epitran/test_vietnamese.py
@@ -13,40 +13,32 @@ class TestNormalizePunc(unittest.TestCase):
 
     def test_tieng(self):
         tr = self.epi.transliterate('tiếng')
-        print(tr)
         self.assertEqual(tr, 'tiə̯ŋ')
 
     def test_viet(self):
         tr = self.epi.transliterate('Việt')
-        print(tr)
         self.assertEqual(tr, 'viə̯t')
 
     def test_ngang(self):
         tr = self.epi.transliterate('ngang')
-        print(tr)
         self.assertEqual(tr, 'ŋaːŋ')
 
     def test_huyen(self):
         tr = self.epi.transliterate('huyền')
-        print(tr)
         self.assertEqual(tr, 'hwiə̯n')
 
     def test_sac(self):
         tr = self.epi.transliterate('sắc')
-        print(tr)
         self.assertEqual(tr, 'sak')
 
     def test_hoi(self):
         tr = self.epi.transliterate('hỏi')
-        print(tr)
-        self.assertEqual(tr, 'hoj')
+        self.assertEqual(tr, 'hɔj')
 
     def test_nga(self):
         tr = self.epi.transliterate('ngã')
-        print(tr)
-        self.assertEqual(tr, 'ŋɑː')
+        self.assertEqual(tr, 'ŋaː')
 
     def test_nang(self):
         tr = self.epi.transliterate('nặng')
-        print(tr)
-        self.assertEqual(, 'naŋ')
+        self.assertEqual(tr, 'naŋ')
