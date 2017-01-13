daf0df79360e24cad34bdbc0356308afc6e96fb5
David R. Mortensen <davidmortensen@gmail.com>
2017-01-13 17:07:13 -0500

    Added Punjabi support

---
+++ epitran/test_punjabi.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import unittest
+
+import _epitran
+
+
+class TestPunjabi(unittest.TestCase):
+    def setUp(self):
+        self.epi = _epitran.Epitran(u'pan-Guru')
+
+    def test_lahore(self):
+        tr = self.epi.transliterate('ਲਹੌਰ')
+        self.assertEqual(tr, 'ləɦɔɾ')
+
+    def test_pakistani(self):
+        tr = self.epi.transliterate('ਪਾਕਿਸਤਾਨੀ')
+        self.assertEqual(tr, 'pɑkɪst̪ɑni')
+
+    def test_panjab(self):
+        tr = self.epi.transliterate('ਪੰਜਾਬ')
+        self.assertEqual(tr, 'pə̃d͡ʒɑb')
+
+    def test_rajdani(self):
+        tr = self.epi.transliterate('ਰਾਜਧਾਨੀ')
+        self.assertEqual(tr, 'ɾɑd͡ʒt̪ɑni')
+
+    def test_lok(self):
+        tr = self.epi.transliterate('ਲੋਕ')
+        self.assertEqual(tr, 'lok')
+
+    def test_ginti(self):
+        tr = self.epi.transliterate('ਗਿਣਤੀ')
+        self.assertEqual(tr, 'ɡɪɳt̪i')
+
+    def test_nal(self):
+        tr = self.epi.transliterate('ਨਾਲ')
+        self.assertEqual(tr, 'nɑl')
