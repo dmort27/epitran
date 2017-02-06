9e95549141b1a7b6ae1c7599184e0d0fbd7cd8a7
David R. Mortensen <davidmortensen@gmail.com>
2017-02-06 15:26:00 -0500

    Corrected missing sys

--- epitran/flite.py
+++ epitran/flite.py
@@ -12,6 +12,7 @@ import regex as re
 import unicodecsv as csv
 from ligaturize import ligaturize
 
+import sys
 if os.name == 'posix' and sys.version_info[0] < 3:
     import subprocess32 as subprocess
 else:
