1a05d1c40b706d8e496bf938f19eabb1c2e6bc21
David R. Mortensen <davidmortensen@gmail.com>
2017-02-07 09:49:52 -0500

    Updated tests for Python 3

--- epitran/test/test_flite.py
+++ epitran/test/test_flite.py
@@ -1,50 +1,17 @@
 #!/usr/bin/env Python
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
 import unittest
-import epitran.flite
-import types
+from epitran import flite
 
 
-class TestVectorWithIPASpace(unittest.TestCase):
+class TestFlite(unittest.TestCase):
     def setUp(self):
-        self.vwis = flite.VectorsWithIPASpace()
+        self.flite = flite.Flite()
 
-    def test_data_structure1(self):
-        for seg in self.vwis.word_to_segs('DRY-dipped'):
-            self.assertEqual(len(seg), 6)
-            self.assertIsInstance(seg[0], types.StringTypes)
-            self.assertIsInstance(seg[1], types.IntType)
-            self.assertIsInstance(seg[2], types.StringTypes)
-            self.assertIsInstance(seg[3], types.StringTypes)
-            self.assertIsInstance(seg[4], types.IntType)
-            self.assertIsInstance(seg[5], types.ListType)
-            self.assertTrue(len(seg[5]) == self.vwis.num_panphon_fts)
-            for v in seg[5]:
-                self.assertIn(v, [-1, 0, 1])
+    def test_san_leandro(self):
+        self.assertEqual(self.flite.transliterate('San Leandro'), 'sæn liɑndɹow')
 
-    def test_data_structure2(self):
-        for seg in self.vwis.word_to_segs('$variable'):
-            self.assertEqual(len(seg), 6)
-            self.assertIsInstance(seg[0], types.StringTypes)
-            self.assertIsInstance(seg[1], types.IntType)
-            self.assertIsInstance(seg[2], types.StringTypes)
-            self.assertIsInstance(seg[3], types.StringTypes)
-            self.assertIsInstance(seg[4], types.IntType)
-            self.assertIsInstance(seg[5], types.ListType)
-            self.assertTrue(len(seg[5]) == self.vwis.num_panphon_fts)
-            for v in seg[5]:
-                self.assertIn(v, [-1, 0, 1])
-
-    def test_data_structure3(self):
-        for seg in self.vwis.word_to_segs('San Leandro'):
-            self.assertEqual(len(seg), 6)
-            self.assertIsInstance(seg[0], types.StringTypes)
-            self.assertIsInstance(seg[1], types.IntType)
-            self.assertIsInstance(seg[2], types.StringTypes)
-            self.assertIsInstance(seg[3], types.StringTypes)
-            self.assertIsInstance(seg[4], types.IntType)
-            self.assertIsInstance(seg[5], types.ListType)
-            self.assertTrue(len(seg[5]) == self.vwis.num_panphon_fts)
-            for v in seg[5]:
-                self.assertIn(v, [-1, 0, 1])
+    def test_parowan(self):
+        self.assertEqual(self.flite.transliterate('Parowan'), 'pɛɹ̩awən')
