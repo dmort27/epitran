faf0b64e40db32a82401cb7a475f97be552eb695
David R. Mortensen <davidmortensen@gmail.com>
2017-03-09 17:02:07 -0500

    Preliminary test for Swedish

---
+++ epitran/test/test_swedish.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import unittest
+
+import epitran
+
+
+class TestSwedish(unittest.TestCase):
+    def setUp(self):
+        self.epi = epitran.Epitran('swe-Latn')
+
+    def test_rattstafning(self):
+        tr = self.epi.transliterate('rättstafning')
+        self.assertEqual(tr, 'rɛttstɐfnɪŋ')
