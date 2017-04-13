8b853e183e7c8f5b773a43305fa4456c98b81964
David R. Mortensen <davidmortensen@gmail.com>
2017-04-13 13:56:17 -0400

    Removed dead code from VectorWithIPASpace

--- epitran/vector.py
+++ epitran/vector.py
@@ -1,83 +1,18 @@
-from __future__ import print_function, unicode_literals, division, absolute_import
-import os.path
-from types import ListType, StringTypes
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 import logging
 
-import pkg_resources
-
-import unicodecsv as csv
 from epitran import Epitran
+from epitran.space import Space
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 class VectorsWithIPASpace(object):
-    def __init__(self, code, space_name):
+    def __init__(self, code, space_names):
         self.epi = Epitran(code)
-        # This (singelton) usage is Deprectated.
-        if isinstance(space_name, StringTypes):
-            logging.warn('Singleton spaces are deprecated. Pass space as list.')
-            self.space = self._load_single_space(space_name)
-        elif isinstance(space_name, ListType):
-            self.space = self._load_union_space(space_name)
-        else:
-            raise TypeError('Argument space_name must be string or list.')
-
-    def _load_single_space(self, space_name):
-        # Deprectated.
-        space_fn = os.path.join('data', 'space', space_name + '.csv')
-        space_fn = pkg_resources.resource_filename(__name__, space_fn)
-        with open(space_fn, 'rb') as f:
-            reader = csv.reader(f, encoding='utf-8')
-            return {seg: num for (num, seg) in reader}
-
-    def _load_union_space(self, space_names):
-        segs = set()
-        scripts = list(set([nm.split('-')[1] for nm in space_names]))
-        punc_fns = ['punc-{}.csv'.format(sc) for sc in scripts]
-        for punc_fn in punc_fns:
-            punc_fn = os.path.join('data', 'space', punc_fn)
-            punc_fn = pkg_resources.resource_filename(__name__, punc_fn)
-            with open(punc_fn, 'rb') as f:
-                reader = csv.reader(f, encoding='utf-8')
-                for (mark,) in reader:
-                    segs.add(mark)
-        for name in space_names:
-            fn = os.path.join('data', 'space', name + '.csv')
-            fn = pkg_resources.resource_filename(__name__, fn)
-            with open(fn, 'rb') as f:
-                reader = csv.reader(f, encoding='utf-8')
-                for _, to_ in reader:
-                    for seg in self.epi.ft.segs(to_):
-                        segs.add(seg)
-        enum = enumerate(sorted(list(segs)))
-        return {seg: num for num, seg in enum}
-
-    def _load_union_space_ternary(self, space_names):
-
-        def recode_ft(ft):
-            val2trn = {'+': '2', '0': '1', '-': '0'}
-            try:
-                return val2trn[ft]
-            except KeyError:
-                raise ValueError("Feature values must be '+', '-', or '0'.")
-
-        def vec2ternary(vec):
-            return ''.join(map(recode_ft, vec))
-
-        def to_int(seg):
-            return int(vec2ternary(self.epi.ft.segment_to_vector(seg)), base=3)
-
-        segs = {}
-        for name in space_names:
-            fn = os.path.join('data', name + '.csv')
-            fn = pkg_resources.resource_filename(__name__, fn)
-            with open(fn, 'rb') as f:
-                reader = csv.reader(f, encoding='utf-8')
-                for _, to_ in reader:
-                    for seg in self.epi.ft.segs(to_):
-                        segs[seg] = to_int(seg)
-        return segs
+        self.space = Space(code, space_names)
 
     def word_to_segs(self, word, normpunc=False):
         """Returns feature vectors, etc. for segments and punctuation in a word.
