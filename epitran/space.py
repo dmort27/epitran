fa3b750a2c312bbb6f9e42b3811a96fb5deabe80
David R. Mortensen <davidmortensen@gmail.com>
2017-04-13 13:57:07 -0400

    Made Space iterable

--- epitran/space.py
+++ epitran/space.py
@@ -6,18 +6,60 @@ import os
 
 import pkg_resources
 import unicodecsv as csv
+from epitran import Epitran
 
 
 class Space(object):
-    def __init__(self, space_name):
-        self.dict = self._load_space(space_name)
+    def __init__(self, code, space_names):
+        """Construct a Space object
+
+        Space objects take strings (corresponding to segments) and return
+        integers, placing them in an integer space that can be translated into
+        a one-hot vector.
+
+        Args:
+            code (str): ISO 639-3 code joined to ISO 15924 code with "-"
+            space_names (list): list of space names consisting of ISO 639-3
+            codes joined to ISO 15924 codes with "-"
+        """
+        self.epi = Epitran(code)
+        self.dict = self._load_space(space_names)
+
+    def _load_space(self, space_names):
+        segs = set()
+        scripts = list(set([nm.split('-')[1] for nm in space_names]))
+        punc_fns = ['punc-{}.csv'.format(sc) for sc in scripts]
+        for punc_fn in punc_fns:
+            punc_fn = os.path.join('data', 'space', punc_fn)
+            punc_fn = pkg_resources.resource_filename(__name__, punc_fn)
+            with open(punc_fn, 'rb') as f:
+                reader = csv.reader(f, encoding='utf-8')
+                for (mark,) in reader:
+                    segs.add(mark)
+        for name in space_names:
+            fn = os.path.join('data', 'space', name + '.csv')
+            fn = pkg_resources.resource_filename(__name__, fn)
+            with open(fn, 'rb') as f:
+                reader = csv.reader(f, encoding='utf-8')
+                for _, to_ in reader:
+                    for seg in self.epi.ft.segs(to_):
+                        segs.add(seg)
+        enum = enumerate(sorted(list(segs)))
+        return {seg: num for num, seg in enum}
+
+    def __iter__(self):
+        return iter(self.dict)
 
     def __getitem__(self, key):
-        return self.dict[key]
+        """Given a string as a key, return the corresponding integer
 
-    def _load_space(self, space_name):
-        space_fn = os.path.join('data', 'space', space_name + '.csv')
-        space_fn = pkg_resources.resource_filename(__name__, space_fn)
-        with open(space_fn, 'rb') as f:
-            reader = csv.reader(f, encoding='utf-8')
-            return {seg: int(num) for (num, seg) in reader}
+        Args:
+            key (unicode): a unicode key corresponding to a segment
+
+        Returns:
+            int: the integer corresponding to the unicode string
+        """
+        try:
+            return self.dict[key]
+        except KeyError:
+            return len(self.dict)
