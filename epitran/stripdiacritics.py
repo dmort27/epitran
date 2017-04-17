abd786d215b618be57cb294c0a0265edb0535cf1
David R. Mortensen <davidmortensen@gmail.com>
2017-04-17 10:45:43 -0400

    Added Google-style docstrings

--- epitran/stripdiacritics.py
+++ epitran/stripdiacritics.py
@@ -10,6 +10,11 @@ import pkg_resources
 
 class StripDiacritics(object):
     def __init__(self, code):
+        """Constructs object to strip specified diacritics from text
+
+        Args:
+            code (str): ISO 639-3 code and ISO 15924 code joined with a hyphen
+        """
         self.diacritics = self._read_diacritics(code)
 
     def _read_diacritics(self, code):
@@ -27,6 +32,14 @@ class StripDiacritics(object):
         return diacritics
 
     def process(self, word):
+        """Remove diacritics from an input string
+
+        Args:
+            word (unicode): Unicode IPA string
+
+        Returns:
+            unicode: Unicode IPA string with specified diacritics removed
+        """
         word = unicodedata.normalize('NFD', word)
         for diacritic in self.diacritics:
             word = word.replace(diacritic, '')
