c3e12ce8434e6cbb9f38c9480c7b6068fb8cfedb
David R. Mortensen <davidmortensen@gmail.com>
2017-05-02 14:11:06 -0400

    Updated rule migration script, correcting errors

--- epitran/bin/migraterules.py
+++ epitran/bin/migraterules.py
@@ -5,7 +5,7 @@ from __future__ import (print_function, unicode_literals, absolute_import)
 
 import glob
 import re
-import codecs
+import io
 
 import unicodecsv
 
@@ -13,19 +13,26 @@ import unicodecsv
 def build_rule(fields):
     try:
         a, b, X, Y = fields
+        b = 0 if not b else b
+        a = 0 if not a else a
+        return '{} -> {} / {} _ {}'.format(a, b, X, Y)
     except ValueError:
         print('Malformed rule: {}'.format(','.join(fields)))
-    return '{} -> {} / {} _ {}'.format(a, b, X, Y)
 
 
-def main(globex):
+def main():
     for csv in glob.glob('*.csv'):
-        txt = csv[:-4] + '.txt'
-        with open(csv, 'rb') as f, codecs.open(txt, 'w', 'utf-8') as g:
+        txt = re.match('[A-Za-z-]+', csv).group(0) + '.txt'
+        with open(csv, 'rb') as f, io.open(txt, 'w', encoding='utf-8') as g:
             reader = unicodecsv.reader(f, encoding='utf-8')
+            next(reader)
             for fields in reader:
-                if re.match('\s%', fields[0]):
+                if re.match('\s*%', fields[0]):
+                    print(','.join([x for x in fields if x]), file=g)
+                else:
                     rule = build_rule(fields)
+                    rule = re.sub('[ ]+', ' ', rule)
+                    rule = re.sub('[ ]$', '', rule)
                     print(rule, file=g)
 
 
