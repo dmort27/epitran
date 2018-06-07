365812414e517ad75e49c21900758efda16ec5d0
David R. Mortensen <davidmortensen@gmail.com>
2018-06-07 17:12:42 -0400

    Updated version to 0.47

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='epitran',
-      version='0.46',
+      version='0.47',
       description='Tools for transcribing languages into IPA.',
       url='http://github.com/dmort27/epitran',
-      download_url='http://github.com/dmort27/epitran/tarball/0.46',
+      download_url='http://github.com/dmort27/epitran/tarball/0.47',
       author='David R. Mortensen',
       author_email='dmortens@cs.cmu.edu',
       license='MIT',
