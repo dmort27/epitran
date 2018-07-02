611a6d926ea8fb9a2185961da54917de99728c20
David R. Mortensen <davidmortensen@gmail.com>
2018-07-02 13:40:26 -0400

    Fix Sinhala bugs

---
+++ epitran/test/test_sinhala.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+
+import unittest
+import unicodedata
+
+import epitran
+
+
+class TestSinhalaGeneral(unittest.TestCase):
+    def setUp(self):
+        self.epi = epitran.Epitran(u'sin-Sinh')
+
+    def _assert_trans(self, src, tar):
+        trans = self.epi.transliterate(src)
+        trans = unicodedata.normalize('NFD', trans)
+        src = unicodedata.normalize('NFD', trans)
+        # print('{}\t{}\t{}'.format(trans, tar, zip(trans, tar)))
+        self.assertEqual(trans, tar)
+
+    def test_alapila(self):
+        self._assert_trans('ඇලපිල්ල', 'ælapilla')
+
+    def test_(self):
+        self._assert_trans('කොම්බුව', 'kombuva')
