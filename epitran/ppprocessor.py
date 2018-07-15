3a22758b3176e2d9720d9a19b27dab8338c22eea
Ilgiz Mustafin <ilgimustafin@gmail.com>
2018-07-15 03:24:09 +0300

    Reverse Russian transcription, upd. Russian tests to current behaviour

--- epitran/ppprocessor.py
+++ epitran/ppprocessor.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class PrePostProcessor(object):
-    def __init__(self, code, fix):
+    def __init__(self, code, fix, rev):
         """Constructs a pre/post-processor for orthographic/IPA strings
 
         This class reads processor files consisting of context-sensitive rules
@@ -24,11 +24,13 @@ class PrePostProcessor(object):
         Args:
             code (str): ISO 639-3 code and ISO 15924 code joined with a hyphen
             fix (str): 'pre' for preprocessors, 'post' for postprocessors
+            rev (boolean): True for reverse transliterating pre/post-processors
         """
-        self.rules = self._read_rules(code, fix)
+        self.rules = self._read_rules(code, fix, rev)
 
-    def _read_rules(self, code, fix):
+    def _read_rules(self, code, fix, rev):
         assert fix in ['pre', 'post']
+        code += '_rev' if rev else ''
         fn = os.path.join('data', fix, code + '.txt')
         try:
             abs_fn = pkg_resources.resource_filename(__name__, fn)
