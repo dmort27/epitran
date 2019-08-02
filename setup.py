9cf8c66f35804550aca3757955e533ceb796114a
David R. Mortensen <dmortens@cs.cmu.edu>
2019-08-02 12:16:56 -0400

    Bumped version up to 1.01

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from io import open
 
 setup(name='epitran',
-      version='1.00',
+      version='1.01',
       description='Tools for transcribing languages into IPA.',
       long_description=open('README.md', encoding='utf-8').read(),
       long_description_content_type='text/markdown',
