61c755f2f6b79036e973891481a4d94a054f1737
David R. Mortensen <dmortens@cs.cmu.edu>
2021-11-11 20:51:55 -0500

    Bumped version again

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from io import open
 
 setup(name='epitran',
-      version='1.14',
+      version='1.15',
       description='Tools for transcribing languages into IPA.',
       long_description=open('README.md', encoding='utf-8').read(),
       long_description_content_type='text/markdown',
       url='http://github.com/dmort27/epitran',
-      download_url='https://github.com/dmort27/epitran/archive/1.14.tar.gz',
+      download_url='https://github.com/dmort27/epitran/archive/1.15.tar.gz',
       author='David R. Mortensen',
       author_email='dmortens@cs.cmu.edu',
       license='MIT',
