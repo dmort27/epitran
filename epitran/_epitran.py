e54375eb09af37be618219799df3de603436fce2
David R. Mortensen <dmortens@cs.cmu.edu>
2022-07-06 22:23:01 +0200

    Removed Py2 support and added Sphinx docstrings

--- epitran/_epitran.py
+++ epitran/_epitran.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
 import logging
 import sys
+from typing import Union
 
 import panphon.featuretable
 from epitran.epihan import Epihan, EpihanTraditional
@@ -12,35 +10,29 @@ from epitran.puncnorm import PuncNorm
 from epitran.simple import SimpleEpitran
 from epitran.xsampa import XSampa
 
-if sys.version_info[0] == 3:
-    def unicode(x):
-        return x
-
 logger = logging.getLogger('epitran')
 logger.setLevel(logging.WARNING)
 
 class Epitran(object):
-    """Unified interface for IPA transliteration/transcription"""
+    """Unified interface for IPA transliteration/transcription
+
+    :param code str: ISO 639-3 plus "-" plus ISO 15924 code of the language/script pair that should be loaded
+    :param preproc bool: apply preprocessors
+    :param postproc bool:  apply postprocessors
+    :param ligatures bool: use precomposed ligatures instead of standard IPA
+    :param cedict_filename str: path to file containing the CC-CEDict dictionary
+    :param rev boolean: use reverse transliteration
+    :param rev_preproc bool: if True, apply preprocessors when reverse transliterating
+    :param rev_postproc bool: if True, apply postprocessors when reverse transliterating 
+    """
     special = {'eng-Latn': FliteLexLookup,
                'cmn-Hans': Epihan,
                'cmn-Hant': EpihanTraditional}
 
-    def __init__(self, code, preproc=True, postproc=True, ligatures=False, cedict_file=None,
-                 rev=False, rev_preproc=True, rev_postproc=True, tones=False):
-        """Construct Epitran transliteration/transcription object
-
-        Args:
-            code (str): ISO 639-3 plus "-" plus ISO 15924 code of the
-                        language/script pair that should be loaded
-            preproc (bool): apply preprocessors
-            postproc (bool): apply prostprocessors
-            ligatures (bool): use precomposed ligatures instead of standard IPA
-            cedict_filename (str): path to file containing the CC-CEDict
-                                   dictionary; relevant only for Chinese
-            rev (boolean): if True, load reverse transliteration
-            rev_preproc (bool): if True, apply preprocessor when reverse transliterating
-            rev_postproc (bool): if True, apply postprocessor when reverse transliterating
-        """
+    def __init__(self, code: str, preproc: bool=True, postproc: bool=True, ligatures: bool=False,
+                cedict_file: Union[bool, None]=None, rev: bool=False, 
+                rev_preproc: bool=True, rev_postproc: bool=True, tones: bool=False):
+        """Constructor method"""
         if code in self.special:
             self.epi = self.special[code](ligatures=ligatures, cedict_file=cedict_file, tones=tones)
         else:
@@ -49,93 +41,88 @@ class Epitran(object):
         self.xsampa = XSampa()
         self.puncnorm = PuncNorm()
 
-    def transliterate(self, word, normpunc=False, ligatures=False):
+    def transliterate(self, word: str, normpunc: bool=False, ligatures: bool=False) -> str:
         """Transliterates/transcribes a word into IPA
 
-        Args:
-            word (str): word to transcribe; unicode string
-            normpunc (bool): normalize punctuation
-            ligatures (bool): use precomposed ligatures instead of standard IPA
-
-        Returns:
-            unicode: IPA string
+        :param word str: word to transcribe
+        :param normpunc bool: if True, normalize punctuation
+        :param ligatures bool: if True, use precomposed ligatures instead of standard IPA
+        :return: An IPA string corresponding to the input orthographic string
+        :rtype: str
         """
         return self.epi.transliterate(word, normpunc, ligatures)
 
-    def reverse_transliterate(self, ipa):
+    def reverse_transliterate(self, ipa: str) -> str:
         """Reconstructs word from IPA. Does the reverse of transliterate()
 
-        Args:
-            ipa (str): word transcription in ipa; unicode string
-
-        Returns:
-            unicode: reconstructed word
+        :param ipa str: An IPA representation of a word
+        :return: An orthographic representation of the word
+        :rtype: str
         """
         return self.epi.reverse_transliterate(ipa)
 
-    def strict_trans(self, word, normpunc=False, ligatures=False):
+    def strict_trans(self, word: str, normpunc:bool =False, ligatures: bool=False) -> str:
+        """Transliterate a word into IPA, ignoring all characters that cannot be recognized.
+
+        :param word str: word to transcribe
+        :param normpunc bool, optional: if True, normalize punctuation
+        :param ligatures bool, optional: if True, use precomposed ligatures instead of standard IPA
+        :return: An IPA string corresponding to the input orthographic string, with all uncoverted characters omitted
+        :rtype: str
+        """
         return self.epi.strict_trans(word, normpunc, ligatures)
 
-    def trans_list(self, word, normpunc=False, ligatures=False):
+    def trans_list(self, word: str, normpunc: bool=False, ligatures: bool=False) -> "list[str]":
         """Transliterates/transcribes a word into list of IPA phonemes
 
-        Args:
-            word (str): word to transcribe; unicode string
-            normpunc (bool): normalize punctuation
-            ligatures (bool): use precomposed ligatures instead of standard IPA
-
-        Returns:
-            list: list of IPA strings, each corresponding to a segment
+        :param word str: word to transcribe
+        :param normpunc bool, optional: if True, normalize punctuation
+        :param ligatures bool, optional: if True, use precomposed ligatures instead of standard IPA
+        :return: list of IPA strings, each corresponding to a segment
+        :rtype: list[str]
         """
         return self.ft.segs_safe(self.epi.transliterate(word, normpunc, ligatures))
 
-    def trans_delimiter(self, text, delimiter=str(' '), normpunc=False, ligatures=False):
+    def trans_delimiter(self, text: str, delimiter: str=str(' '), normpunc: bool=False, ligatures: bool=False):
         """Return IPA transliteration with a delimiter between segments
 
-        Args:
-            text (unicode): orthographic text
-            delimiter (str): string to insert between segments
-            normpunc (bool): if True, normalize punctation down
-            ligatures (bool): if True, use phonetic ligatures for affricates
-                              instead of standard IPA
-        Returns:
-            unicode: transliteration with segments delimited by `delimiter`
+        :param text str: An orthographic text
+        :param delimiter str, optional: A string to insert between segments
+        :param normpunc bool, optional: If True, normalize punctuation
+        :param ligatures bool, optional: If True, use precomposed ligatures instead of standard IPA
+        :return: String of IPA phonemes separated by `delimiter`
+        :rtype: str
         """
         return delimiter.join(self.trans_list(text, normpunc=normpunc,
                                               ligatures=ligatures))
 
-    def xsampa_list(self, word, normpunc=False, ligaturize=False):
+    def xsampa_list(self, word: str, normpunc: bool=False, ligaturize: bool=False):
         """Transliterates/transcribes a word as X-SAMPA
 
-        Args:
-            word (str): word to transcribe; unicode string
-            normpunc (bool): normalize punctuation
-            ligatures (bool): use precomposed ligatures instead of standard IPA
-
-        Returns:
-            list: X-SAMPA strings, each corresponding to a segment
+        :param word str: An orthographic word
+        :param normpunc bool, optional: If True, normalize punctuation
+        :param ligatures bool, optional: If True, use precomposed ligatures instead of standard IPA
+        :return: List of X-SAMPA strings corresponding to `word`
+        :rtype: list[str]
         """
         ipa_segs = self.ft.ipa_segs(self.epi.strict_trans(word, normpunc,
                                                           ligaturize))
         return list(map(self.xsampa.ipa2xs, ipa_segs))
 
-    def word_to_tuples(self, word, normpunc=False, ligaturize=False):
-        """Given a word, returns a list of tuples corresponding to IPA segments.
-
-        Args:
-            word (unicode): word to transliterate
-            normpunc (bool): If True, normalizes punctuation to ASCII inventory
-
-        Returns:
-        list: A list of (category, lettercase, orthographic_form, phonetic_form,
-              fecture_vectors) tuples.
-
-        The "feature vectors" form a list consisting of (segment, vector) pairs.
+    def word_to_tuples(self, word: str, normpunc: bool=False, ligaturize: bool=False):
+        """Given a word, returns a list of tuples corresponding to IPA segments. The "feature
+        vectors" form a list consisting of (segment, vector) pairs.
         For IPA segments, segment is a substring of phonetic_form such that the
         concatenation of all segments in the list is equal to the phonetic_form.
         The vectors are a sequence of integers drawn from the set {-1, 0, 1}
         where -1 corresponds to '-', 0 corresponds to '0', and 1 corresponds to
         '+'.
+
+        :param word str: An orthographic word
+        :param normpunc bool, optional: If True, normalize punctuation
+        :param ligatures bool, optional: If True, use precomposed ligatures instead of standard IPA
+        :return: A list of tuples corresponding to IPA segments
+        :rtype: list[tuple[str, str, str, str, list[int]]]
         """
         try:
             return self.epi.word_to_tuples(word, normpunc)
