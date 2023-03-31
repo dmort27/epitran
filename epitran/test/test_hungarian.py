c50a2b93640e2fa32d7244888785e4e86719939f
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-03-31 19:03:10 +0200

    [skip ci] updated Hungarian ng and added tests

--- epitran/test/test_hungarian.py
+++ epitran/test/test_hungarian.py
@@ -21,11 +21,23 @@ class TestHungarianGeneral(unittest.TestCase):
     def test_fiaei(self):
         self._assert_trans('fiáéi', 'fiaːeːi')
 
+    def test_fiaiei(self):
+        self._assert_trans('fiaiéi', 'fiɒieːi')
+
     def test_baratnoje(self):
         self._assert_trans('barátnője', 'bɒraːtnøːjɛ')
 
     def test_magyar(self):
         self._assert_trans('magyar', 'mɒɟɒr')
 
-    def test_(self):
-        self._assert_trans('nagyszülő', 'nɒɟsyløː')
+    def test_nagyszulo(self):
+        self._assert_trans('nagyszülő', 'nɒɟsyløː')  # is actually pronounced as nat͡sːyløː
+
+    def test_hang(self):
+        self._assert_trans('hang', 'hɒŋ')
+
+    def test_henger(self):
+        self._assert_trans('henger', 'hɛŋɡɛr')
+
+    def test_hangtan(self):
+        self._assert_trans('hangtan', 'hɒŋtɒn')
