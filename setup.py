4251a7202d71ca921f491fe4b847b3eec4090f6f
David R. Mortensen <dmortens@cs.cmu.edu>
2025-01-22 22:21:22 -0500

    Incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from io import open
 from setuptools import setup
 
 setup(name='epitran',
-      version='1.25.1',
+      version='1.26.0',
       description='Tools for transcribing languages into IPA.',
       long_description=open('README.md', encoding='utf-8').read(),
       long_description_content_type='text/markdown',
