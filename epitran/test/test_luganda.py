b70ce11de0fb16f381d29d26d97b72d8ffea7841
aglassoforange <yliu2911@gmail.com>
2025-03-18 22:52:55 -0400

    update test for luganda

--- epitran/test/test_luganda.py
+++ epitran/test/test_luganda.py
@@ -5,18 +5,50 @@ class TestLuganda(unittest.TestCase):
     def setUp(self):
         self.epi = Epitran('lug-Latn')
     
-    def test_basic_conversions(self):
-        self.assertEqual(self.epi.transliterate('a'), 'a')
-        self.assertEqual(self.epi.transliterate('b'), 'b')
-    
-    def test_long_vowels(self):
-        self.assertEqual(self.epi.transliterate('aa'), 'aː')
-        self.assertEqual(self.epi.transliterate('ee'), 'eː')
-    
-    def test_prenasalized_consonants(self):
-        self.assertEqual(self.epi.transliterate('mb'), 'ᵐb')
-        self.assertEqual(self.epi.transliterate('nd'), 'ⁿd')
-    
-    def test_words(self):
-        self.assertEqual(self.epi.transliterate('omuntu'), 'omuntu')
-        self.assertEqual(self.epi.transliterate('eggulu'), 'eɡːulu')
+    def test_basic_consonant(self):
+        tr = self.epi.transliterate('baba')
+        self.assertEqual(tr, 'baba')
+        
+    def test_prenasalized(self):
+        tr = self.epi.transliterate('mbwa')
+        self.assertEqual(tr, 'ᵐbʷa')
+        
+    def test_long_vowel(self):
+        tr = self.epi.transliterate('baaba')
+        self.assertEqual(tr, 'baːba')
+        
+    def test_palatal_nasal(self):
+        tr = self.epi.transliterate('nyama')
+        self.assertEqual(tr, 'ɲama')
+        
+    def test_velar_nasal(self):
+        tr = self.epi.transliterate('ng\'amba')
+        self.assertEqual(tr, 'ŋaᵐba')
+        
+    def test_labialized(self):
+        tr = self.epi.transliterate('ekitwala')
+        self.assertEqual(tr, 'ekitʷala')
+        
+    def test_geminated(self):
+        tr = self.epi.transliterate('attabi')
+        self.assertEqual(tr, 'atːabi')
+        
+    def test_affricate(self):
+        tr = self.epi.transliterate('jjajja')
+        self.assertEqual(tr, 'dʒːadʒːa')
+        
+    def test_prenasalized_affricate(self):
+        tr = self.epi.transliterate('njovu')
+        self.assertEqual(tr, 'ⁿdʒovu')
+        
+    def test_vowel_length_before_prenasalized(self):
+        tr = self.epi.transliterate('abantu')
+        self.assertEqual(tr, 'abaːⁿtu')
+        
+    def test_compound_word(self):
+        tr = self.epi.transliterate('eggulu')
+        self.assertEqual(tr, 'eɡːulu')
+        
+    def test_complex_word(self):
+        tr = self.epi.transliterate('kyaddala')
+        self.assertEqual(tr, 'tʃadːala')
