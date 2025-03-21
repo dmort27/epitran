21a9458eec240831332bf5d1bfd58580da47d487
chrisdasht4 <christofish2@gmail.com>
2025-03-21 15:10:58 -0400

    Basic consonant variation handled

--- epitran/test/test_irish.py
+++ epitran/test/test_irish.py
@@ -15,7 +15,7 @@ class TestIrish(unittest.TestCase):
         pred = self.epi.transliterate(orth)
         self.assertEqual(pred,phon)
 
-    # Test broad vowels
+    # Test broad/slender vowels
     def test_ao_ae(self):
         pairs = [
             ('saol',u'siːl'),
@@ -56,7 +56,15 @@ class TestIrish(unittest.TestCase):
             self.assertEqual(pred,phon)
         
     def test_eo_exceptions(self):
-        ...
+        pairs = [
+            (u'anseo',u'anʃo'),
+            (u'deoch',u'dʲox'),
+            (u'eochair',u'oxaɾʲ'),
+            (u'seo',u'ʃo'),
+        ]
+        for orth,phon in pairs:
+            pred = self.epi.transliterate(orth)
+            self.assertEqual(pred,phon)
 
     def test_e_ea(self):
         pairs = [
@@ -100,7 +108,7 @@ class TestIrish(unittest.TestCase):
             (u'cinnte',u'cinʲtʲe'),
             (u'im',u'imʲ'),
             (u'faoistin',u'fiːʃtʲinʲ'),
-            (u'gnímh',u'ɟnʲiːvʲ'),
+            (u'gnímh',u'ɟɾʲiːvʲ'),
             (u'cailín',u'kalʲiːnʲ'),
             (u'síol',u'ʃiːl'),
             (u'fios',u'fʲis'),
@@ -187,7 +195,7 @@ class TestIrish(unittest.TestCase):
             (u'oird',u'oːɾdʲ'),
             (u'anois',u'aniʃ'),
             (u'gloine',u'glinʲe'),
-            (u'cnoic',u'knic'),
+            (u'cnoic',u'kɾic'),
             (u'roimh',u'ɾivʲ'),
             (u'coimeád',u'kimʲaːd'),
             (u'loinge',u'liɲɟe'),
@@ -241,6 +249,105 @@ class TestIrish(unittest.TestCase):
             (u'aguisín',u'agiʃiːnʲ'),
             (u'buígh',u'biːj'),
             (u'buíon',u'biːn'),
+        ]
+        for orth,phon in pairs:
+            pred = self.epi.transliterate(orth)
+            self.assertEqual(pred,phon)
+
+    # Test consonants with predictable changes in pronunciation
+    def test_dt(self):
+        # <dt> is realized as /d/ as eclipsis, but /t/ elsewhere
+        pairs = [
+            (u'dtaisce',u'daʃce'),
+            (u'greadta',u'ɟɾʲata'),
+            (u'dtír',u'dʲiːɾʲ'),
+            (u'goidte',u'getʲe'),
+        ]
+        for orth,phon in pairs:
+            pred = self.epi.transliterate(orth)
+            self.assertEqual(pred,phon)
+
+    def test_dh_gh(self):
+        pairs = [
+            (u'dhorn',u'ɣoːɾn'),
+            (u'ádh',u'aː'),
+            #(u'dhearg',u'jaɾəg'), # schwa-epenthesis
+            (u'fáidh',u'faːj'),
+            (u'ghasúr',u'ɣasuːɾ'),
+            (u'Eoghan',u'oːan'),
+            (u'gheata',u'jata'),
+            (u'dóigh',u'doːj'),
+        ]
+        for orth,phon in pairs:
+            pred = self.epi.transliterate(orth)
+            self.assertEqual(pred,phon)
+
+    def test_n_clusters(self):
+        pairs = [
+            (u'naoi',u'niː'),
+            (u'nótaí',u'noːtiː'),
+            (u'mná',u'mɾaː'),
+            (u'cnaipe',u'kɾapʲe'),
+            (u'neart',u'nʲaɾt'),
+            (u'gnéas',u'ɟɾʲeːs'),
+            (u'cníopaire',u'cɾʲiːpaɾʲe'),
+            (u'Eoin',u'oːnʲ'),
+            (u'ngasúr',u'ŋasuːɾ'),
+            (u'teanga',u'tʲaŋga'),
+            (u'ngeata',u'ɲata'),
+            (u'cuing',u'kiɲɟ'),
+            (u'ingear',u'iɲɟaɾ'),
+            (u'ceann',u'can'),
+            (u'tinneas',u'tʲinʲas'),
+        ]
+        for orth,phon in pairs:
+            pred = self.epi.transliterate(orth)
+            self.assertEqual(pred,phon)
+        
+    def test_r(self):
+        pairs = [
+            (u'ruán',u'ɾuːaːn'),
+            #(u'cumhra',u'kuːɾa'), #final <mh>
+            (u'fuar',u'fuəɾ'),
+            (u'rí',u'ɾiː'),
+            (u'airde',u'aːɾdʲe'),
+            #(u'duirling',u'duːɾlʲənʲ'),
+            (u'coirnéal',u'koːɾnʲeːl'),
+            (u'cuairt',u'kuəɾtʲ'),
+            (u'sreang',u'sɾaŋg'),
+            (u'tirim',u'tʲiɾʲimʲ'),
+            (u'fuair',u'fuəɾʲ'),
+            (u'carr',u'kaːɾ'),
+        ]
+        for orth,phon in pairs:
+            pred = self.epi.transliterate(orth)
+            self.assertEqual(pred,phon)
+
+    def test_th(self):
+        pairs = [
+            #(u'thuaidh',u'huə'), # final <dh>
+            (u'thíos',u'hiːs'),
+            (u'athair',u'ahaɾʲ'),
+            (u'coinnithe',u'kinʲihe'),
+            (u'ith',u'ih'),
+            #(u'foghlamtha',u'foːlamha'), # final <gh>
+            (u'ruaigthe',u'ɾuəce'),
+            (u'scuabtha',u'skuəpa'),
+            (u'bláth',u'blaː'),
+            (u'cliath',u'clʲiə'),
+        ]
+        for orth,phon in pairs:
+            pred = self.epi.transliterate(orth)
+            self.assertEqual(pred,phon)
+
+    def test_loan_consonants(self):
+        pairs = [
+            (u'jab',u'd͡ʒab'),
+            (u'jíp',u'd͡ʒiːpʲ'),
+            (u'vóta',u'woːta'),
+            (u'veidhlín',u'vʲəilʲiːnʲ'),
+            (u'zú',u'zuː'),
+            (u'Zen',u'ʒɛnʲ'),
         ]
         for orth,phon in pairs:
             pred = self.epi.transliterate(orth)
