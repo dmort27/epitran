d15c41bcb01452a410706c61f6c1f125f2b1dae0
John Ternullo <jternull@andrew.cmu.edu>
2025-03-25 00:05:26 -0400

    Add Frisian support

---
+++ epitran/test/test_frisian.py
@@ -0,0 +1,35 @@
+# Sources include Wiktionary and Frysk Hânwurdboek
+
+from __future__ import print_function, unicode_literals
+
+import unittest
+
+import epitran
+
+
+def assemble_ipa(xs):
+    return ''.join([x[3] for x in xs])
+
+
+class TestDutch(unittest.TestCase):
+    def setUp(self):
+        self.epi = epitran.Epitran('fry-Latn')
+
+    def _derivation(self, orth, correct):
+        attempt = assemble_ipa(self.epi.word_to_tuples(orth))
+        self.assertEqual(attempt, correct)
+
+    def test_wurde(self):
+        self._derivation('wurde', 'vødə')
+
+    def test_alden(self):
+        self._derivation('âlden', 'ɔːdən')
+
+    def test_wenne(self):
+        self._derivation('wenne', 'vɛnə')
+
+    def test_tsjelke(self):
+        self._derivation('tsjelke', 'tsjɛlkə')
+
+    def test_skriuwe(self):
+        self._derivation('skriuwe', 'skrjoːu̯ə')
