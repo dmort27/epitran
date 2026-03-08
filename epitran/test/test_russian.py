407dd1e5c3dbaf4357263508dc55220afce4cd8e
chrisdasht4 <christofish2@gmail.com>
2026-03-08 12:33:46 -0700

    New tests and phonetic rules for Russian

--- epitran/test/test_russian.py
+++ epitran/test/test_russian.py
@@ -20,4 +20,50 @@ class TestRussian(unittest.TestCase):
 
     def test_vzglat(self):
         tr = self.epi.transliterate('взгляд')
-        self.assertEqual(tr, 'vzɡlʲad')
+        self.assertEqual(tr, 'vzɡlʲat')
+
+    def test_exceptions(self):
+        """Lexical exceptions to general phonetic rules."""
+        tr = self.epi.transliterate('сегодня')
+        self.assertEqual(tr, 'sʲivodnʲa')
+        tr = self.epi.transliterate('бог')
+        self.assertEqual(tr, 'box')
+
+    def test_g_softening(self):
+        """The letter г is pronounced as /v/ or /x/ in certain regular contexts."""
+        pairs = [
+            ('легко', 'lʲexko'),
+            ("мягкий", 'mʲaxkij'),
+            ('легчать', 'lʲext͡ɕʲatʲ'),
+            ("чего", 't͡ɕʲevo'),
+            ("кого", 'kovo')
+        ]
+        for orth, phon in pairs:
+            tr = self.epi.transliterate(orth)
+            self.assertEqual(tr, phon)
+
+    def test_voicing_assimilation(self):
+        pairs = [
+            ('все', 'fsʲe'),
+            ('встретить', 'fstrʲetʲitʲ'),
+            ("рассказ", 'raskas'),
+            ("сделать", 'zdʲelatʲ'),
+            ("просьба", 'prozʲba'),
+            ("водка", 'votka'),
+        ]
+        for orth, phon in pairs:
+            tr = self.epi.transliterate(orth)
+            self.assertEqual(tr, phon)
+
+    def test_hushers(self):
+        pairs = [
+            ('жить', 'ʒɨtʲ'),
+            ('шить', 'ʂɨtʲ'),
+            ("цифра", 't͡sɨfra'),
+        ]
+        for orth, phon in pairs:
+            tr = self.epi.transliterate(orth)
+            self.assertEqual(tr, phon)
+
+if __name__ == '__main__':
+    unittest.main()
